de940f425150da53d53721a2ee1996d4bb98aadb
Emilis Baliukonis <emasbakas@gmail.com>
2019-11-09 22:37:23 +0200

    Initial Commit

---
+++ Blockchain2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Blockchain2
+//
+//  Created by Emilis Baliukonis on 2019-11-09.
+//  Copyright © 2019 Emilis Baliukonis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
